d139abfaac18c3a646e1474e2605f4bae52b346b
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-01-15 18:09:45 -0500

    started the condition model

--- src/test_condition.py
+++ src/test_condition.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
+from datasets import load_dataset
 
 @dataclass
 class TrainingConfig:
-    image_size: int = 128  # the generated image resolution
-    train_batch_size: int = 64
+    image_size: int = 28  # the generated image resolution
+    train_batch_size: int = 16
     eval_batch_size: int = 16  # how many images to sample during evaluation
     num_epochs: int = 2
     gradient_accumulation_steps: int = 1
@@ -12,7 +13,7 @@ class TrainingConfig:
     save_image_epochs: int = 1
     save_model_epochs: int = 1
     mixed_precision: str = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
-    output_dir: str = "ddpm-butterflies-128"  # the model name locally and on the HF Hub
+    output_dir: str = "mnist-model"  # the model name locally and on the HF Hub
     push_to_hub: bool = False # whether to upload the saved model to the HF Hub
     hub_model_id: str = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
     hub_private_repo: bool = False
@@ -24,10 +25,8 @@ config = TrainingConfig()
 
 print(config)
 
-from datasets import load_dataset
 
-config.dataset_name = "huggan/smithsonian_butterflies_subset"
-dataset = load_dataset(config.dataset_name, split="train")
+dataset = load_dataset('mnist', split='train')
 
 import matplotlib.pyplot as plt
 
@@ -42,14 +41,13 @@ from torchvision import transforms
 preprocess = transforms.Compose(
     [
         transforms.Resize((config.image_size, config.image_size)),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5]),
     ]
 )
 
 def transform(examples):
-    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    images = [preprocess(image) for image in examples["image"]]
     return {"images": images}
 
 
@@ -65,8 +63,8 @@ from diffusers import UNet2DModel
 # Assuming config is defined as in your previous message
 model = UNet2DModel(
     sample_size=config.image_size,  # the target image resolution
-    in_channels=3,  # the number of input channels, 3 for RGB images
-    out_channels=3,  # the number of output channels
+    in_channels=1,  # the number of input channels, 3 for RGB images
+    out_channels=1,  # the number of output channels
     layers_per_block=2,  # how many ResNet layers to use per UNet block
     block_out_channels=(128, 128, 256, 256, 512, 512),  # output channels for each UNet block
     down_block_types=(
@@ -103,7 +101,7 @@ noise = torch.randn(sample_image.shape)
 timesteps = torch.LongTensor([50])
 noisy_image = noise_scheduler.add_noise(sample_image, noise, timesteps)
 
-Image.fromarray(((noisy_image.permute(0, 2, 3, 1) + 1.0) * 127.5).type(torch.uint8).numpy()[0])
+# Image.fromarray(((noisy_image.permute(0, 2, 3, 1) + 1.0) * 127.5).type(torch.uint8).numpy()[0])
 
 import torch.nn.functional as F
 
@@ -196,6 +194,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
 
         for step, batch in enumerate(train_dataloader):
             clean_images = batch["images"].to(device)
+            print(clean_images.size())
             # Sample noise to add to the images
             noise = torch.randn(clean_images.shape, device=device)
             
