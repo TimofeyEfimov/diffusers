e3d681540128fddbe280dfbebafd4865f3b7fbee
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-01-16 13:24:05 -0500

    finally works

--- src/test_condition.py
+++ src/test_condition.py
@@ -6,7 +6,7 @@ from diffusers import UNet2DConditionModel
 @dataclass
 class TrainingConfig:
     image_size: int = 128 # the generated image resolution
-    train_batch_size: int = 256
+    train_batch_size: int = 2
     eval_batch_size: int = 16  # how many images to sample during evaluation
     num_epochs: int = 10
     gradient_accumulation_steps: int = 1
@@ -29,7 +29,42 @@ config = TrainingConfig()
 print(config)
 
 
-dataset = load_dataset('mnist', split='train')
+# dataset = load_dataset('mnist', split='train')
+
+# import matplotlib.pyplot as plt
+
+# fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+# for i, image in enumerate(dataset[:4]["image"]):
+#     axs[i].imshow(image)
+#     axs[i].set_axis_off()
+# fig.show()
+
+# from torchvision import transforms
+
+# preprocess = transforms.Compose(
+#     [
+#         transforms.Resize((config.image_size, config.image_size)),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.5], [0.5]),
+#     ]
+# )
+
+# def transform(examples):
+#     images = [preprocess(image) for image in examples["image"]]
+#     labels = examples["label"]
+#     return {"images": images, "labels": labels}
+
+
+# dataset.set_transform(transform)
+
+# import torch
+
+# train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+
+from datasets import load_dataset
+
+config.dataset_name = "huggan/smithsonian_butterflies_subset"
+dataset = load_dataset(config.dataset_name, split="train")
 
 import matplotlib.pyplot as plt
 
@@ -44,14 +79,15 @@ from torchvision import transforms
 preprocess = transforms.Compose(
     [
         transforms.Resize((config.image_size, config.image_size)),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5]),
     ]
 )
 
 def transform(examples):
-    images = [preprocess(image) for image in examples["image"]]
-    labels = examples["label"]
+    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    labels = examples["sim_score"]
     return {"images": images, "labels": labels}
 
 
@@ -86,13 +122,11 @@ from diffusers import UNet2DModel
 # )
 
 model = UNet2DConditionModel(
-    sample_size=config.image_size,  # the target image resolution
-    in_channels=1,  # the number of input channels, 3 for RGB images
-    out_channels=1,  # the number of output channels
-    layers_per_block=3,  # how many ResNet layers to use per UNet block
+    in_channels = 3,
+    out_channels=3,
+    layers_per_block=2, 
     block_out_channels=(64, 64, 64, 64),
-    encoder_hid_dim = 10,
-    cross_attention_dim=10  # output channels for each UNet block
+      # output channels for each UNet block
 )
 
 # Sample image from your dataset for testing
@@ -184,6 +218,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
         model, optimizer, train_dataloader, lr_scheduler
     )
 
+
     # model = torch.nn.DataParallel(model)
     # model.to(device)
 
@@ -209,7 +244,8 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
             clean_images = batch["images"].to(device)
             labels = batch["labels"].to(device)
             labels = labels.unsqueeze(1)
-            labels= labels.repeat(1, 1, 10)
+            labels= labels.repeat(2, 1, 1280)
+            #labels = labels.expand(-1, -1, 1280)
             labels = labels.to(torch.float32)
             labels = labels.to(device)
             #print(labels.size())
@@ -235,8 +271,9 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
 
             with accelerator.accumulate(model):
                 # Predict the noise residual
-                # print(noisy_images.size(), timesteps.size(), labels.size())
-                
+                #print(noisy_images.size(), timesteps, labels.size())
+                #print(model)
+
                 noise_pred = model(noisy_images, timesteps, labels, return_dict=False)[0]
                 #print(noise_pred.size())
                 loss = F.mse_loss(noise_pred, noise)
@@ -279,7 +316,7 @@ from accelerate import notebook_launcher
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = torch.nn.DataParallel(model)
+# model = torch.nn.DataParallel(model)
 model = model.to(device)
 print(device)
 
