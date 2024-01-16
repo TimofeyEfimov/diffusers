41bb5d031cdec9b26af8567688790b431cac72fd
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-01-16 13:38:49 -0500

    I think finally fixed batch size problem

--- src/test_condition.py
+++ src/test_condition.py
@@ -6,7 +6,7 @@ from diffusers import UNet2DConditionModel
 @dataclass
 class TrainingConfig:
     image_size: int = 128 # the generated image resolution
-    train_batch_size: int = 2
+    train_batch_size: int = 16
     eval_batch_size: int = 16  # how many images to sample during evaluation
     num_epochs: int = 10
     gradient_accumulation_steps: int = 1
@@ -29,42 +29,7 @@ config = TrainingConfig()
 print(config)
 
 
-# dataset = load_dataset('mnist', split='train')
-
-# import matplotlib.pyplot as plt
-
-# fig, axs = plt.subplots(1, 4, figsize=(16, 4))
-# for i, image in enumerate(dataset[:4]["image"]):
-#     axs[i].imshow(image)
-#     axs[i].set_axis_off()
-# fig.show()
-
-# from torchvision import transforms
-
-# preprocess = transforms.Compose(
-#     [
-#         transforms.Resize((config.image_size, config.image_size)),
-#         transforms.ToTensor(),
-#         transforms.Normalize([0.5], [0.5]),
-#     ]
-# )
-
-# def transform(examples):
-#     images = [preprocess(image) for image in examples["image"]]
-#     labels = examples["label"]
-#     return {"images": images, "labels": labels}
-
-
-# dataset.set_transform(transform)
-
-# import torch
-
-# train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
-
-from datasets import load_dataset
-
-config.dataset_name = "huggan/smithsonian_butterflies_subset"
-dataset = load_dataset(config.dataset_name, split="train")
+dataset = load_dataset('mnist', split='train')
 
 import matplotlib.pyplot as plt
 
@@ -79,15 +44,14 @@ from torchvision import transforms
 preprocess = transforms.Compose(
     [
         transforms.Resize((config.image_size, config.image_size)),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5]),
     ]
 )
 
 def transform(examples):
-    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
-    labels = examples["sim_score"]
+    images = [preprocess(image) for image in examples["image"]]
+    labels = examples["label"]
     return {"images": images, "labels": labels}
 
 
@@ -96,6 +60,42 @@ dataset.set_transform(transform)
 import torch
 
 train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+
+# from datasets import load_dataset
+
+# config.dataset_name = "huggan/smithsonian_butterflies_subset"
+# dataset = load_dataset(config.dataset_name, split="train")
+
+# import matplotlib.pyplot as plt
+
+# fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+# for i, image in enumerate(dataset[:4]["image"]):
+#     axs[i].imshow(image)
+#     axs[i].set_axis_off()
+# fig.show()
+
+# from torchvision import transforms
+
+# preprocess = transforms.Compose(
+#     [
+#         transforms.Resize((config.image_size, config.image_size)),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.5], [0.5]),
+#     ]
+# )
+
+# def transform(examples):
+#     images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+#     labels = examples["sim_score"]
+#     return {"images": images, "labels": labels}
+
+
+# dataset.set_transform(transform)
+
+# import torch
+
+# train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
 
 
 from diffusers import UNet2DModel
@@ -122,8 +122,8 @@ from diffusers import UNet2DModel
 # )
 
 model = UNet2DConditionModel(
-    in_channels = 3,
-    out_channels=3,
+    in_channels = 1,
+    out_channels=1,
     layers_per_block=2, 
     block_out_channels=(64, 64, 64, 64),
       # output channels for each UNet block
@@ -243,9 +243,11 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
             
             clean_images = batch["images"].to(device)
             labels = batch["labels"].to(device)
+            print("Labels size is", labels.size())
             labels = labels.unsqueeze(1)
-            labels= labels.repeat(2, 1, 1280)
+            labels= labels.repeat(1, 1280)
             #labels = labels.expand(-1, -1, 1280)
+            labels = labels.unsqueeze(1)
             labels = labels.to(torch.float32)
             labels = labels.to(device)
             #print(labels.size())
@@ -271,7 +273,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
 
             with accelerator.accumulate(model):
                 # Predict the noise residual
-                #print(noisy_images.size(), timesteps, labels.size())
+                print(noisy_images.size(), timesteps.size(), labels.size())
                 #print(model)
 
                 noise_pred = model(noisy_images, timesteps, labels, return_dict=False)[0]
