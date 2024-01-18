a761a685903be40d6fb8adb7ad0b954bb2ee1898
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-01-18 16:44:37 -0500

    forgot to change the optimizer

--- src/new_cifar.py
+++ src/new_cifar.py
@@ -16,7 +16,7 @@ class TrainingConfig:
     save_model_epochs: int = 1
     mixed_precision: str = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
     output_dir: str = "cifar-model"  # the model name locally and on the HF Hub
-    push_to_hub: bool = False # whether to upload the saved model to the HF Hub
+    push_to_hub: bool = False # whether to upload 32the saved model to the HF Hub
     hub_model_id: str = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
     hub_private_repo: bool = False
     overwrite_output_dir: bool = True  # overwrite the old model when re-running the notebook
@@ -142,9 +142,140 @@ from tqdm.auto import tqdm
 from pathlib import Path
 import os
 
+modelV = UNet2DModel(
+    sample_size=config.image_size,  # the target image resolution
+    in_channels=3,  # the number of input channels, 3 for RGB images
+    out_channels=3,  # the number of output channels
+    freq_shift=1,
+    flip_sin_to_cos = False,
+
+    layers_per_block=2,  # how many ResNet layers to use per UNet block
+    block_out_channels=(128, 256, 256, 256),  # output channels for each UNet block
+    down_block_types=(
+        "DownBlock2D",  # a regular ResNet downsampling block
+        "DownBlock2D",
+        "AttnDownBlock2D",  # a ResNet block with spatial self-attention
+        "DownBlock2D",
+    ),
+    up_block_types=(
+        "UpBlock2D",  # a regular ResNet upsampling block
+        "AttnUpBlock2D",  # a ResNet block with spatial self-attention
+        "UpBlock2D",
+        "UpBlock2D",
+    ),
+)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+modelV = torch.nn.DataParallel(modelV)
+modelV = modelV.to(device)
 
 
-def train_loop(config, model, modelV, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    #Initialize accelerator and tensorboard logging
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        log_with="tensorboard",
+        project_dir=os.path.join(config.output_dir, "logs")
+    )
+
+    # model = torch.nn.DataParallel(model)
+    # model.to(device)
+    if accelerator.is_main_process:
+        if config.output_dir is not None:
+            os.makedirs(config.output_dir, exist_ok=True)
+        if config.push_to_hub:
+            repo_id = create_repo(
+                repo_id=config.hub_model_id or Path(config.output_dir).name, exist_ok=True
+            ).repo_id
+        
+
+    # Prepare everything
+    # There is no specific order to remember, you just need to unpack the
+    # objects in the same order you gave them to the prepare method.
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
+
+    # model = torch.nn.DataParallel(model)
+    # model.to(device)
+
+    first_param = next(model.parameters()).device
+    if isinstance(model, torch.nn.DataParallel):
+        print("Model is on multiple GPUs:", model.device_ids)
+    else:
+        print("Model is on single device:", next(model.parameters()).device)
+    # Assuming 'accelerator' is your Accelerator object
+    print("Accel Devices:", accelerator.device_placement)
+
+    global_step = 0
+
+    # Now you train the model
+    for epoch in range(config.num_epochs):
+        print(epoch)
+        start_time = time.time() 
+        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar.set_description(f"Epoch {epoch}")
+
+        for step, batch in enumerate(train_dataloader):
+            
+            clean_images = batch["images"].to(device)
+            
+            
+            #print(clean_images.size())
+            # Sample noise to add to the images
+            noise = torch.randn(clean_images.shape, device=device)
+            # second_noise = torch.randn(clean_images.shape, device=device)
+            
+            
+            bs = clean_images.shape[0]
+
+            # Sample a random timestep for each image
+            timesteps = torch.randint(
+                0, noise_scheduler.config.num_train_timesteps, (bs,), device=device,
+                dtype=torch.int64
+            )
+
+            # Add noise to the clean images according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+            #print(noisy_images.size())
+
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                #merged_img2 = torch.cat((noisy_images, noisy_images), dim=1)
+                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+
+                #print(noise_pred.size())
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
+
+                accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+        progress_bar.update(1)
+        logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+        progress_bar.set_postfix(**logs)
+        accelerator.log(logs, step=global_step)
+        global_step += 1
+
+
+        end_time = time.time() 
+        epoch_duration = end_time - start_time  # Calculate the duration of the epoch
+        print(f"Epoch {epoch} completed in {epoch_duration:.2f} seconds")
+
+    return model
+
+optimizer = torch.optim.AdamW(modelV.parameters(), lr=config.learning_rate)
+modelV = train_loop(config, modelV, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+
+
+
+def train_loop_main(config, model, modelV, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
@@ -152,6 +283,7 @@ def train_loop(config, model, modelV, noise_scheduler, optimizer, train_dataload
     for param in modelV.parameters():
         param.requires_grad = False
     modelV.eval()
+
     #Initialize accelerator and tensorboard logging
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
@@ -240,7 +372,7 @@ def train_loop(config, model, modelV, noise_scheduler, optimizer, train_dataload
         global_step += 1
 
         if accelerator.is_main_process:
-            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), modelV=None, scheduler=noise_scheduler)
+            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), modelV=modelV, scheduler=noise_scheduler)
 
             if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
                 evaluate(config, epoch, pipeline)
@@ -269,33 +401,9 @@ model = torch.nn.DataParallel(model)
 model = model.to(device)
 print(device)
 
-modelV = UNet2DModel(
-    sample_size=config.image_size,  # the target image resolution
-    in_channels=6,  # the number of input channels, 3 for RGB images
-    out_channels=3,  # the number of output channels
-    freq_shift=1,
-    flip_sin_to_cos = False,
 
-    layers_per_block=2,  # how many ResNet layers to use per UNet block
-    block_out_channels=(128, 256, 256, 256),  # output channels for each UNet block
-    down_block_types=(
-        "DownBlock2D",  # a regular ResNet downsampling block
-        "DownBlock2D",
-        "AttnDownBlock2D",  # a ResNet block with spatial self-attention
-        "DownBlock2D",
-    ),
-    up_block_types=(
-        "UpBlock2D",  # a regular ResNet upsampling block
-        "AttnUpBlock2D",  # a ResNet block with spatial self-attention
-        "UpBlock2D",
-        "UpBlock2D",
-    ),
-)
 
-modelV = torch.nn.DataParallel(modelV)
-modelV = modelV.to(device)
-
-train_loop(config, model, modelV, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+train_loop_main(config, model, modelV, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
 
 
 print("Hi i am here")
