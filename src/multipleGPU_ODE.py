953cacf5150783e7ace0be4da4438367d159a623
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-01-30 01:17:24 -0500

    Added the remaining codes

--- src/multipleGPU_ODE.py
+++ src/multipleGPU_ODE.py
@@ -6,16 +6,17 @@ import numpy as np
 import os 
 from accelerate import PartialState
 from diffusers import ConsistencyModelPipeline
+import time 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model_id = "google/ddpm-ema-celebahq-256"
+model_id = "google/ddpm-ema-bedroom-256"
 
 
 
 ddpm = DDPMPipeline.from_pretrained(model_id)
 print(ddpm.config)
 
-steps = 750
+steps = 700
 noise_scheduler = DDPMScheduler()
 # Load the model and scheduler
 ddpm = DDPMPipeline.from_pretrained(model_id)
@@ -26,8 +27,9 @@ unet = UNet2DModel.from_pretrained(
     model_id
 )
 
+
 seed1, seed2, seed3 = 1,2,3
-pipeline = DDPMPipeline(unet=unet, scheduler=noise_scheduler)
+pipeline = DDPMPipeline.from_pretrained(model_id)
 
 print(pipeline.scheduler.config)
 distributed_state = PartialState()
@@ -36,17 +38,471 @@ pipeline.to(distributed_state.device)
 print(pipeline.config)
 
 # Generate images
-batch_size = 30
+batch_size = 50
+seed1, seed2, seed3, seed4 = 0,1,2,3
 
 # Save each image in the batch
-output_dir = "/home/tefimov/diffusers/src/NewSamplers/CelebaEMA/90samples_NewSDE750/"
-
+output_dir = "/home/tefimov/diffusers/src/NewSamplers/Bedroom/1000_NewSDE700/"
 os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
-with distributed_state.split_between_processes([seed1, seed2, seed3]) as prompt:
-    print("prompt is", prompt[0])
-    images11 = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
-    for i, image in enumerate(images11):
-        image_path = os.path.join(output_dir, f"{distributed_state.process_index}_{i}.png")
-        image.save(image_path)
-        print(f"Saved {image_path}")
 
+
+num_iterations = 5
+for iteration in range(num_iterations):
+    print(output_dir)
+    print("It is for 800")
+    start_time = time.time()
+    seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+    with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+        print("prompt is", prompt[0])
+        images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+        for i, image in enumerate(images):
+            image_number = (iteration+0) * batch_size * 5 + i
+            image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+            image.save(image_path)
+            print(f"Saved {image_path}")
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    print(f"Time for one iteration: {iteration_time} seconds")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "google/ddpm-ema-bedroom-256"
+
+
+
+ddpm = DDPMPipeline.from_pretrained(model_id)
+print(ddpm.config)
+
+steps = 800
+noise_scheduler = DDPMScheduler()
+# Load the model and scheduler
+ddpm = DDPMPipeline.from_pretrained(model_id)
+from diffusers import UNet2DModel
+
+
+unet = UNet2DModel.from_pretrained(
+    model_id
+)
+
+
+seed1, seed2, seed3 = 1,2,3
+pipeline = DDPMPipeline.from_pretrained(model_id)
+
+print(pipeline.scheduler.config)
+distributed_state = PartialState()
+pipeline.to(distributed_state.device)
+
+print(pipeline.config)
+
+# Generate images
+batch_size = 50
+seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# Save each image in the batch
+output_dir = "/home/tefimov/diffusers/src/NewSamplers/Bedroom/1000samples_NewSDE800/"
+os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+num_iterations = 5*2
+for iteration in range(num_iterations):
+    print("It is for 800")
+
+    start_time = time.time()
+    seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+    with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+        print("prompt is", prompt[0])
+        images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+        for i, image in enumerate(images):
+            image_number = (iteration+0) * batch_size * 5 + i
+            image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+            image.save(image_path)
+            print(f"Saved {image_path}")
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    print(f"Time for one iteration: {iteration_time} seconds")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "google/ddpm-ema-bedroom-256"
+
+
+
+ddpm = DDPMPipeline.from_pretrained(model_id)
+print(ddpm.config)
+
+steps = 900
+noise_scheduler = DDPMScheduler()
+# Load the model and scheduler
+ddpm = DDPMPipeline.from_pretrained(model_id)
+from diffusers import UNet2DModel
+
+
+unet = UNet2DModel.from_pretrained(
+    model_id
+)
+
+
+seed1, seed2, seed3 = 1,2,3
+pipeline = DDPMPipeline.from_pretrained(model_id)
+
+print(pipeline.scheduler.config)
+distributed_state = PartialState()
+pipeline.to(distributed_state.device)
+
+print(pipeline.config)
+
+# Generate images
+batch_size = 50
+seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# Save each image in the batch
+output_dir = "/home/tefimov/diffusers/src/NewSamplers/Bedroom/1000samples_NewSDE900/"
+os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+num_iterations = 5*2
+for iteration in range(num_iterations):
+    print("It is for 800")
+    start_time = time.time()
+    seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+    with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+        print("prompt is", prompt[0])
+        images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+        for i, image in enumerate(images):
+            image_number = (iteration+0) * batch_size * 5 + i
+            image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+            image.save(image_path)
+            print(f"Saved {image_path}")
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    print(f"Time for one iteration: {iteration_time} seconds")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "google/ddpm-ema-bedroom-256"
+
+
+
+ddpm = DDPMPipeline.from_pretrained(model_id)
+print(ddpm.config)
+
+steps = 1000
+noise_scheduler = DDPMScheduler()
+# Load the model and scheduler
+ddpm = DDPMPipeline.from_pretrained(model_id)
+from diffusers import UNet2DModel
+
+
+unet = UNet2DModel.from_pretrained(
+    model_id
+)
+
+
+seed1, seed2, seed3 = 1,2,3
+pipeline = DDPMPipeline.from_pretrained(model_id)
+
+print(pipeline.scheduler.config)
+distributed_state = PartialState()
+pipeline.to(distributed_state.device)
+
+print(pipeline.config)
+
+# Generate images
+batch_size = 50
+seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# Save each image in the batch
+output_dir = "/home/tefimov/diffusers/src/NewSamplers/Bedroom/1000samples_NewSDE1000/"
+os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+num_iterations = 5*2
+for iteration in range(num_iterations):
+    print("It is for 800")
+    start_time = time.time()
+    seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+    with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+        print("prompt is", prompt[0])
+        images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+        for i, image in enumerate(images):
+            image_number = (iteration+0) * batch_size * 5 + i
+            image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+            image.save(image_path)
+            print(f"Saved {image_path}")
+    end_time = time.time()
+    iteration_time = end_time - start_time
+    print(f"Time for one iteration: {iteration_time} seconds")
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "google/ddpm-ema-church-256"
+
+
+
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# print(ddpm.config)
+
+# steps = 700
+# noise_scheduler = DDPMScheduler()
+# # Load the model and scheduler
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# from diffusers import UNet2DModel
+
+
+# unet = UNet2DModel.from_pretrained(
+#     model_id
+# )
+
+
+# seed1, seed2, seed3 = 1,2,3
+# pipeline = DDPMPipeline.from_pretrained(model_id)
+
+# print(pipeline.scheduler.config)
+# distributed_state = PartialState()
+# pipeline.to(distributed_state.device)
+
+# print(pipeline.config)
+
+# # Generate images
+# batch_size = 50
+# seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# # Save each image in the batch
+# output_dir = "/home/tefimov/diffusers/src/NewSamplers/ChurchesEMA/1000samples_VanillaODE700/"
+# os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+# num_iterations = 5
+# for iteration in range(num_iterations):
+#     print("It is for 800")
+#     start_time = time.time()
+#     seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+#     with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+#         print("prompt is", prompt[0])
+#         images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+#         for i, image in enumerate(images):
+#             image_number = (iteration+0) * batch_size * 5 + i
+#             image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+#             image.save(image_path)
+#             print(f"Saved {image_path}")
+#     end_time = time.time()
+#     iteration_time = end_time - start_time
+#     print(f"Time for one iteration: {iteration_time} seconds")
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "google/ddpm-ema-church-256"
+
+
+
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# print(ddpm.config)
+
+# steps = 800
+# noise_scheduler = DDPMScheduler()
+# # Load the model and scheduler
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# from diffusers import UNet2DModel
+
+
+# unet = UNet2DModel.from_pretrained(
+#     model_id
+# )
+
+
+# seed1, seed2, seed3 = 1,2,3
+# pipeline = DDPMPipeline.from_pretrained(model_id)
+
+# print(pipeline.scheduler.config)
+# distributed_state = PartialState()
+# pipeline.to(distributed_state.device)
+
+# print(pipeline.config)
+
+# # Generate images
+# batch_size = 50
+# seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# # Save each image in the batch
+# output_dir = "/home/tefimov/diffusers/src/NewSamplers/ChurchesEMA/1000samples_VanillaODE800/"
+# os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+# num_iterations = 5
+# for iteration in range(num_iterations):
+#     print(output_dir)
+#     print("It is for 800")
+#     start_time = time.time()
+#     seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+#     with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+#         print("prompt is", prompt[0])
+#         images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+#         for i, image in enumerate(images):
+#             image_number = (iteration+0) * batch_size * 5 + i
+#             image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+#             image.save(image_path)
+#             print(f"Saved {image_path}")
+#     end_time = time.time()
+#     iteration_time = end_time - start_time
+#     print(f"Time for one iteration: {iteration_time} seconds")
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "google/ddpm-ema-church-256"
+
+
+
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# print(ddpm.config)
+
+# steps = 900
+# noise_scheduler = DDPMScheduler()
+# # Load the model and scheduler
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# from diffusers import UNet2DModel
+
+
+# unet = UNet2DModel.from_pretrained(
+#     model_id
+# )
+
+
+# seed1, seed2, seed3 = 1,2,3
+# pipeline = DDPMPipeline.from_pretrained(model_id)
+
+# print(pipeline.scheduler.config)
+# distributed_state = PartialState()
+# pipeline.to(distributed_state.device)
+
+# print(pipeline.config)
+
+# # Generate images
+# batch_size = 50
+# seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# # Save each image in the batch
+# output_dir = "/home/tefimov/diffusers/src/NewSamplers/ChurchesEMA/1000samples_VanillaODE900/"
+# os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+# num_iterations = 5
+# for iteration in range(num_iterations):
+#     print("It is for 800")
+#     start_time = time.time()
+#     seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+#     with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+#         print("prompt is", prompt[0])
+#         images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+#         for i, image in enumerate(images):
+#             image_number = (iteration+0) * batch_size * 5 + i
+#             image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+#             image.save(image_path)
+#             print(f"Saved {image_path}")
+#     end_time = time.time()
+#     iteration_time = end_time - start_time
+#     print(f"Time for one iteration: {iteration_time} seconds")
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "google/ddpm-ema-church-256"
+
+
+
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# print(ddpm.config)
+
+# steps = 1000
+# noise_scheduler = DDPMScheduler()
+# # Load the model and scheduler
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# from diffusers import UNet2DModel
+
+
+# unet = UNet2DModel.from_pretrained(
+#     model_id
+# )
+
+
+# seed1, seed2, seed3 = 1,2,3
+# pipeline = DDPMPipeline.from_pretrained(model_id)
+
+# print(pipeline.scheduler.config)
+# distributed_state = PartialState()
+# pipeline.to(distributed_state.device)
+
+# print(pipeline.config)
+
+# # Generate images
+# batch_size = 50
+# seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# # Save each image in the batch
+# output_dir = "/home/tefimov/diffusers/src/NewSamplers/ChurchesEMA/1000samples_VanillaODE1000/"
+# os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+# num_iterations = 5
+# for iteration in range(num_iterations):
+#     print("It is for 800")
+#     start_time = time.time()
+#     seed1, seed2, seed3, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+#     with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+#         print("prompt is", prompt[0])
+#         images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+#         for i, image in enumerate(images):
+#             image_number = (iteration+0) * batch_size * 5 + i
+#             image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+#             image.save(image_path)
+#             print(f"Saved {image_path}")
+#     end_time = time.time()
+#     iteration_time = end_time - start_time
+#     print(f"Time for one iteration: {iteration_time} seconds")
+
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "google/ddpm-cifar10-32"
+
+
+
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# print(ddpm.config)
+
+# steps = 1000
+# noise_scheduler = DDPMScheduler()
+# # Load the model and scheduler
+# ddpm = DDPMPipeline.from_pretrained(model_id)
+# from diffusers import UNet2DModel
+
+
+# unet = UNet2DModel.from_pretrained(
+#     model_id
+# )
+
+
+# seed1, seed2, seed3 = 1,2,3
+# pipeline = DDPMPipeline.from_pretrained(model_id)
+
+# print(pipeline.scheduler.config)
+# distributed_state = PartialState()
+# pipeline.to(distributed_state.device)
+
+# print(pipeline.config)
+
+# # Generate images
+# batch_size = 50
+# seed1, seed2, seed3, seed4 = 0,1,2,3
+
+# # Save each image in the batch
+# output_dir = "/home/tefimov/diffusers/src/NewSamplers/Cifar/2000samples_VanillaODE1000/"
+# os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+# num_iterations = 10
+# for iteration in range(num_iterations):
+#     print("It is for 800")
+#     start_time = time.time()
+#     seed1, seed2, seed, seed4 = seed1+4,seed2+4,seed3+4, seed4+4
+#     with distributed_state.split_between_processes([seed1, seed2, seed3, seed4]) as prompt:
+#         print("prompt is", prompt[0])
+#         images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=steps).images
+#         for i, image in enumerate(images):
+#             image_number = (iteration+0) * batch_size * 5 + i
+#             image_path = os.path.join(output_dir, f"image_{image_number}_seed_{prompt[0]}.png")
+#             image.save(image_path)
+#             print(f"Saved {image_path}")
+#     end_time = time.time()
+#     iteration_time = end_time - start_time
+#     print(f"Time for one iteration: {iteration_time} seconds")
---
+++ src/progressive.py
@@ -0,0 +1,141 @@
+from diffusers import DDPMPipeline
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+import torch 
+import torch.nn as nn
+import numpy as np
+import os 
+from accelerate import PartialState
+from diffusers import ConsistencyModelPipeline
+import time
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "google/ddpm-ema-bedroom-256"
+
+
+
+def create_steps(start, end, step_increment):
+    steps = [i for i in range(start, end + 1, step_increment)]
+    return steps
+
+# Example usage to create steps from 600 to 1000, in increments of 50
+start_step = 600
+end_step = 1000
+step_increment = 50
+steps = create_steps(start_step, end_step, step_increment)
+print(steps)
+
+
+for step in steps:
+    current_steps = step
+    noise_scheduler = DDPMScheduler()
+    # Load the model and scheduler
+    #ddpm = DDPMPipeline.from_pretrained(model_id)
+    from diffusers import UNet2DModel
+
+
+    # unet = UNet2DModel.from_pretrained(
+    #     model_id
+    # )
+
+
+    seed1, seed2, seed3 = 1,2,3
+    pipeline = DDPMPipeline.from_pretrained(model_id)
+
+    print(pipeline.scheduler.config)
+    distributed_state = PartialState()
+    pipeline.to(distributed_state.device)
+
+    print(pipeline.config)
+
+    # Generate images
+    batch_size = 5
+    seed1 = 0
+
+    # Save each image in the batch
+    output_dir = "/home/tefimov/diffusers/src/NewSamplers/ChurchesEMA/ProgressionMore/VanillaSDE_1000/"
+    os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+    num_iterations = 1
+    for iteration in range(num_iterations):
+        start_time = time.time()
+        seed1 = seed1+1
+        with distributed_state.split_between_processes([seed1]) as prompt:
+            print("prompt is", prompt[0])
+            print(current_steps)
+            images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=current_steps).images
+            for i, image in enumerate(images):
+                image_number = (iteration+0) * batch_size * 1 + i
+                image_path = os.path.join(output_dir, f"image_{image_number}_seed_{current_steps}.png")
+                image.save(image_path)
+                print(f"Saved {image_path}")
+        end_time = time.time()
+        iteration_time = end_time - start_time
+        print(f"Time for one iteration: {iteration_time} seconds")
+
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "google/ddpm-ema-bedroom-256"
+
+
+
+def create_steps(start, end, step_increment):
+    steps = [i for i in range(start, end + 1, step_increment)]
+    return steps
+
+# Example usage to create steps from 600 to 1000, in increments of 50
+start_step = 600
+end_step = 1000
+step_increment = 50
+steps = create_steps(start_step, end_step, step_increment)
+print(steps)
+
+
+for step in steps:
+    current_steps = step
+    noise_scheduler = DDPMScheduler()
+    # Load the model and scheduler
+    #ddpm = DDPMPipeline.from_pretrained(model_id)
+    from diffusers import UNet2DModel
+
+
+    # unet = UNet2DModel.from_pretrained(
+    #     model_id
+    # )
+
+
+    seed1, seed2, seed3 = 1,2,3
+    pipeline = DDPMPipeline.from_pretrained(model_id)
+
+    print(pipeline.scheduler.config)
+    distributed_state = PartialState()
+    pipeline.to(distributed_state.device)
+
+    print(pipeline.config)
+
+    # Generate images
+    batch_size = 5
+    seed1 = 0
+
+    # Save each image in the batch
+    output_dir = "/home/tefimov/diffusers/src/NewSamplers/Bedroom/Progression/VanillaSDE_1000/"
+    os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+
+
+    num_iterations = 1
+    for iteration in range(num_iterations):
+        start_time = time.time()
+        seed1 = seed1+1
+        with distributed_state.split_between_processes([seed1]) as prompt:
+            print("prompt is", prompt[0])
+            images = pipeline(batch_size=batch_size,generator=torch.manual_seed(prompt[0]), num_inference_steps=current_steps).images
+            for i, image in enumerate(images):
+                image_number = (iteration+0) * batch_size * 1 + i
+                image_path = os.path.join(output_dir, f"image_{image_number}_seed_{current_steps}.png")
+                image.save(image_path)
+                print(f"Saved {image_path}")
+        end_time = time.time()
+        iteration_time = end_time - start_time
+        print(f"Time for one iteration: {iteration_time} seconds")
+
