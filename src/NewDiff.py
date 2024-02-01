6174c74414b9cbb2c08a4f4d58cae4c4e6152716
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-02-01 01:48:09 -0500

    python code

---
+++ src/NewDiff.py
@@ -0,0 +1,24 @@
+from diffusers import DDPMScheduler, UNet2DModel
+from PIL import Image
+import torch
+import os 
+
+scheduler = DDPMScheduler.from_pretrained("google/ddpm-ema-celebahq-256")
+model = UNet2DModel.from_pretrained("google/ddpm-ema-celebahq-256").to("cuda")
+scheduler.set_timesteps(50)
+
+sample_size = model.config.sample_size
+noise = torch.randn((1, 3, sample_size, sample_size), device="cuda")
+input = noise
+
+for t in scheduler.timesteps:
+    with torch.no_grad():
+        noisy_residual = model(input, t).sample
+        prev_noisy_sample = scheduler.step(noisy_residual, t, input).prev_sample
+        input = prev_noisy_sample
+
+image = (input / 2 + 0.5).clamp(0, 1)
+image = image.cpu().permute(0, 2, 3, 1).numpy()[0]
+image = Image.fromarray((image * 255).round().astype("uint8"))
+
+image.save("TestImages/vanilSDE_50.png")
