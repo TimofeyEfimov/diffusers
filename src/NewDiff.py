ceb341191315ed2694ae892463f47dc49b24ee21
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-02-01 04:51:54 -0500

    New ODE finally works???

--- src/NewDiff.py
+++ src/NewDiff.py
@@ -9,22 +9,26 @@ model = UNet2DModel.from_pretrained("google/ddpm-ema-celebahq-256").to("cuda")
 scheduler.set_timesteps(15)
 # Main code to dynamically generate images for different timesteps
 
-for i in range(5, 200, 5):
+for i in range(100, 130, 10):
     scheduler.set_timesteps(i)
     sample_size = model.config.sample_size
     noise = torch.randn((1, 3, sample_size, sample_size), device="cuda")  # Replace "cpu" with "cuda" if using GPU
     input = noise
 
+    previous_output = None
     for t in scheduler.timesteps:
         with torch.no_grad():
+        
             noisy_residual = model(input, t).sample
-            prev_noisy_sample = scheduler.step(model, noisy_residual, t, input).prev_sample
+            
+            prev_noisy_sample = scheduler.step(model, previous_output, noisy_residual, t, input).prev_sample
             input = prev_noisy_sample
+            previous_output = noisy_residual
 
     image = (input / 2 + 0.5).clamp(0, 1)
     image = image.cpu().permute(0, 2, 3, 1).numpy()[0]
     image = Image.fromarray((image * 255).round().astype("uint8"))
 
-    file_name = f"TestImages/vanillaSDE_{i}.png"
+    file_name = f"TestImages/newODE_{i}.png"
     image.save(file_name)
     print(f"Saved: {file_name}")
