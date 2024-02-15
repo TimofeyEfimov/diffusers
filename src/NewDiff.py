06c61a9171f8754b63fa31551eb860332270d039
TimofeyEfimov <tefimov@andrew.cmu.edu>
2024-02-15 17:00:33 -0500

    Got image reproduction

--- src/NewDiff.py
+++ src/NewDiff.py
@@ -3,6 +3,8 @@ from diffusers import DiffusionPipeline
 from PIL import Image
 import torch
 import os 
+import torch.distributions as dist
+import numpy as np
 
 scheduler = DDPMScheduler.from_pretrained("google/ddpm-ema-celebahq-256")
 model = UNet2DModel.from_pretrained("google/ddpm-ema-celebahq-256").to("cuda")
@@ -16,23 +18,49 @@ model = UNet2DModel.from_pretrained("google/ddpm-ema-celebahq-256").to("cuda")
 scheduler.set_timesteps(15)
 # Main code to dynamically generate images for different timesteps
 
-for i in range(50, 500, 50):
+for i in range(10, 100, 10):
     scheduler.set_timesteps(i)
     sample_size = model.config.sample_size
     noise = torch.randn((1, 3, sample_size, sample_size), device="cuda")  # Replace "cpu" with "cuda" if using GPU
     input = noise
 
-    previous_output = None
-    for t in scheduler.timesteps:
-        with torch.no_grad():
-        
-            noisy_residual = model(input, t).sample
-            
-            prev_noisy_sample = scheduler.step(model, noisy_residual, t, input).prev_sample
-            input = prev_noisy_sample
-            previous_output = noisy_residual
+    p = 0.7  # Probability of success
+    n = 1   
+    binomial_dist = dist.Binomial(total_count=n, probs=p)
 
-    image = (input / 2 + 0.5).clamp(0, 1)
+    # Create matrix A with the same shape as y_data using iid binomial distribution
+    A = binomial_dist.sample(input.size())
+
+    # Multiply the two tensors element-wise
+    #result = sample * A
+    previous_output = None
+    file_name1 = f"TestImages/VanillaSDE/VanillaSDE_300.png"
+    image1 = Image.open(file_name1)
+
+    image_np = np.array(image1)
+
+    # Convert the NumPy array to a PyTorch tensor
+    image_tensor = torch.tensor(image_np.astype("float32") / 255)
+
+    
+    # Undo the permute operation: from (H, W, C) to (C, H, W)
+    image_tensor = image_tensor.permute(2, 0, 1)
+    image_tensor = image_tensor.unsqueeze(0)
+
+    # Undo the normalization: from [0, 1] range to the original tensor before saving
+    input_tensor4 = (image_tensor - 0.5) * 2
+
+
+    # for t in scheduler.timesteps:
+    #     with torch.no_grad():
+        
+    #         noisy_residual = model(input, t).sample
+            
+    #         prev_noisy_sample = scheduler.step(model, noisy_residual, t, input).prev_sample
+    #         input = prev_noisy_sample
+    #         previous_output = noisy_residual
+    
+    image = (input_tensor4 / 2 + 0.5).clamp(0, 1)
     image = image.cpu().permute(0, 2, 3, 1).numpy()[0]
     image = Image.fromarray((image * 255).round().astype("uint8"))
 
